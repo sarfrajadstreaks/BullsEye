5a918b363d9deced55845271a69bf4790911992c
Md Sarfraj Akhtar <mdsarfrajakhtar@gmail.com>
2021-09-08 01:28:44 +0530

    create team page

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  */
 const express=require("express");
 const path = require("path");
+const session = require('express-session');
+const crypto = require('crypto');
 /**
  * App Variables
  */
@@ -15,12 +17,24 @@ const port=process.env.PORT|| 8000;
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","pug");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({secret: 'ssshhhhh'}));
+app.use(express.urlencoded({ extended: true })); 
 /**
  * Routes Definitions
  */
     app.get("/", (req,res)=>{
-        // res.status(200).send("Hello");
         res.render("index", {title:"Home"})
+    })
+    //app.post()
+    app.post("/team", (req,res)=>{
+        sess = req.session;
+        var mykey = crypto.randomUUID({disableEntropyCache : true});
+        var TeamName=req.body.TeamName;
+        var AdminName=req.body.AdminName
+        sess.id=mykey;
+        sess.TeamName=TeamName
+        sess.AdminName=AdminName
+        res.render("board", {name:TeamName})
     })
 /**
  * Server Activation
